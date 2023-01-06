535eb391ba39b32a7fc0955381e9c84a98e0763f
Joel-K-Muraguri <joelkmuraguri21@gmail.com>
2023-01-06 17:12:00 +0300

    firebase auth

--- app/src/main/java/com/example/forage_compose/ForageApp.kt
+++ app/src/main/java/com/example/forage_compose/ForageApp.kt
@@ -1,11 +1,29 @@
 package com.example.forage_compose
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import com.example.forage_compose.worker.NotificationsService
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ForageApp : Application(){
 
+    override fun onCreate() {
+        super.onCreate()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NotificationsService.NOTIFICATION_CHANNEL_ID,
+                "Reminder",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = "Used for forage reminder"
 
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
 
 }
